0245aafb9c07c205036131fa02f819d7a19a2818
Miraç Furkan Durmaz <117925817+mirac2534@users.noreply.github.com>
2024-10-25 08:35:08 +0300

    Update main.c

--- main.c
+++ main.c
@@ -3,11 +3,11 @@
 
 int main()
 {
-double r,area,circ;
+double r,area,circ; // I defined radius, area, and circumference
 printf("enter the radius of the circle:");
-scanf("%lf",&r);
-area=M_PI*r*r;
-circ=M_PI*r*2;
+scanf("%lf",&r); // radius you entered
+area=M_PI*r*r; // calculates the area
+circ=M_PI*r*2; // calculates the circumference 
 printf("the area of circle with radius %.2lf is %2.lf\n",r,area);
 printf("the circumference of circle with radius %.2lf is %.2lf\n",r,circ);
 printf("radius: %.2lf,\n area: %.3lf, \n circumference: %lf",r,area,circ);
